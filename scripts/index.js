a9dedccda91a7a46c5aa708da1c698bcecb5dcb9
Daut Sadykov <sadykov.daut@yandex.ru>
2024-06-27 00:46:08 +0300

    ready for review

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,5 @@
 // @todo: Темплейт карточки
 
-console.log('Hello world!')
-
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
@@ -9,3 +7,32 @@ console.log('Hello world!')
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+const placesList = document.querySelector('.places__list');
+
+function createCard(cardInfo, deleteCard) {
+    const cardTemplate = document.querySelector('#card-template').content
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    
+    cardElement.querySelector('.card__title').textContent = cardInfo.name;
+    cardElement.querySelector('.card__image').src = cardInfo.link;
+    
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', () => {
+        deleteCard(deleteButton)
+    })
+
+    return cardElement
+}
+
+const deleteCard = (deleteButton) => {
+    const cardElement = deleteButton.closest('.card');
+    cardElement.remove();
+}
+
+initialCards.forEach((card) => {
+    placesList.append(createCard(card, deleteCard));
+})
+
+
